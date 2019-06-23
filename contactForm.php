253116bcac448fbdec58fa91da5a54ea1190130d
Arnav <arnavparuthi@gmail.com>
2019-06-23 16:34:29 -0400

    added email list form, updated timeline

---
+++ contactForm.php
@@ -0,0 +1,13 @@
+<?php
+    $subscriberEmail = $_POST['email'];
+
+    $emailFrom = "arnav_paruthi@hotmail.com";
+    $to = "arnavparuthi@gmail.com";
+    $headers = "From: $emailFrom \r\n";
+    $headers .= "Reply-To $subscriberEmail \r\n";
+
+    $emailSubject = "Darwinx Mailing list Subscriber";
+    $emailBody = "$subscriberEmail is a new subscriber";
+
+    mail($to, $emailSubject, $emailBody, $headers );
+?>
