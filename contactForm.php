8ba559d3a156ed68c2ad779967da8cc24decce21
Arnav <arnavparuthi@gmail.com>
2019-06-23 17:06:49 -0400

    trying php again with debugging

--- contactForm.php
+++ contactForm.php
@@ -2,14 +2,15 @@
     $subscriberEmail = $_POST['email'];
 
     $emailFrom = "silen@darwinx.ca";
-    $to = "arnav_paruthi@hotmail.com";
+    $to = "arnav@darwinx.ca";
     $headers = "From: $emailFrom \r\n";
-    $headers .= "Reply-To $subscriberEmail \r\n";
 
     $emailSubject = "Darwinx Mailing list Subscriber";
     $emailBody = "$subscriberEmail is a new subscriber";
 
     mail($to, $emailSubject, $emailBody, $headers );
 
-    header("Location: index.html");
+    header("Location: index.html?mailsent");
+
+    echo "<script>console.log('php script has been excecuted')</script>";
 ?>
